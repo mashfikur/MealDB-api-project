f535bd54cb8c1f45386827b21c7588406591c85e
mashfikur <120697837+mashfikur@users.noreply.github.com>
2023-11-03 19:10:32 +0600

    individual router setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,84 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Foods from "./components/Foods.jsx";
-import ErrorPage from "./components/ErrorPage.jsx";
-import FoodDetails from "./components/FoodDetails.jsx";
-import Layout from "./components/Layout.jsx";
-import Categories from "./components/Categories.jsx";
-import Login from "./components/Login.jsx";
-import Register from "./components/Register.jsx";
 import AuthProvider from "./Authentication/AuthProvider.jsx";
-import Cart from "./components/Cart.jsx";
-import PrivateRoute from "./routes/PrivateRoute.jsx";
+import router from "./routes/Router.jsx";
+import { RouterProvider } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout></Layout>,
-    errorElement: <ErrorPage></ErrorPage>,
-    children: [
-      {
-        path: "/",
-        element: <App></App>,
-      },
-      {
-        path: "/foods",
-        loader: async () => {
-          const res = await fetch(
-            "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
-          );
 
-          const data = await res.json();
-
-          return data;
-        },
-        element: <Foods></Foods>,
-      },
-      {
-        path: "/foods/:foodID",
-        loader: async ({ params }) => {
-          const res = await fetch(
-            `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.foodID}`
-          );
-
-          const data = await res.json();
-
-          return data;
-        },
-        element: <FoodDetails></FoodDetails>,
-      },
-      {
-        path: "/categories",
-        element: <Categories></Categories>,
-        loader: async () => {
-          const res = await fetch(
-            "https://www.themealdb.com/api/json/v1/1/categories.php"
-          );
-          const data = await res.json();
-
-          return data;
-        },
-      },
-      {
-        path: "/register",
-        element: <Register></Register>,
-      },
-      {
-        path: "/login",
-        element: <Login></Login>,
-      },
-      {
-        path: "/cart",
-        element: <PrivateRoute>
-          <Cart></Cart>
-        </PrivateRoute>,
-      },
-    ],
-  },
-]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
---
+++ src/routes/Router.jsx
@@ -0,0 +1,79 @@
+import { createBrowserRouter } from "react-router-dom";
+import Cart from "../components/Cart";
+import PrivateRoute from "./PrivateRoute";
+import Login from "../components/Login";
+import Register from "../components/Register";
+import Categories from "../components/Categories";
+import FoodDetails from "../components/FoodDetails";
+import Foods from "../components/Foods";
+import App from "../App";
+import ErrorPage from "../components/ErrorPage";
+import Layout from "../components/Layout";
+
+const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout></Layout>,
+      errorElement: <ErrorPage></ErrorPage>,
+      children: [
+        {
+          path: "/",
+          element: <App></App>,
+        },
+        {
+          path: "/foods",
+          loader: async () => {
+            const res = await fetch(
+              "https://www.themealdb.com/api/json/v1/1/search.php?f=a"
+            );
+  
+            const data = await res.json();
+  
+            return data;
+          },
+          element: <Foods></Foods>,
+        },
+        {
+          path: "/foods/:foodID",
+          loader: async ({ params }) => {
+            const res = await fetch(
+              `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${params.foodID}`
+            );
+  
+            const data = await res.json();
+  
+            return data;
+          },
+          element: <FoodDetails></FoodDetails>,
+        },
+        {
+          path: "/categories",
+          element: <Categories></Categories>,
+          loader: async () => {
+            const res = await fetch(
+              "https://www.themealdb.com/api/json/v1/1/categories.php"
+            );
+            const data = await res.json();
+  
+            return data;
+          },
+        },
+        {
+          path: "/register",
+          element: <Register></Register>,
+        },
+        {
+          path: "/login",
+          element: <Login></Login>,
+        },
+        {
+          path: "/cart",
+          element: <PrivateRoute>
+            <Cart></Cart>
+          </PrivateRoute>,
+        },
+      ],
+    },
+  ]);
+
+  export default router
